6d11184d6e188c4d9b6e0e99c4feabd37da5332f
Jinwoong <okokrt@gmail.com>
2022-06-25 15:27:37 +0900

    Update light theme

--- lib/style/theme_styles.dart
+++ lib/style/theme_styles.dart
@@ -2,9 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
-    cupertinoOverrideTheme: const CupertinoThemeData(
-  brightness: Brightness.dark,
-  primaryColor: CupertinoColors.systemBlue,
-  textTheme: CupertinoTextThemeData(),
-  barBackgroundColor: CupertinoColors.systemBackground,
-));
+  cupertinoOverrideTheme: const CupertinoThemeData(
+    brightness: Brightness.dark,
+    primaryColor: CupertinoColors.systemBlue,
+    textTheme: CupertinoTextThemeData(),
+    barBackgroundColor: CupertinoColors.systemBackground,
+  ),
+);
+ThemeData lightTheme = ThemeData.light().copyWith(
+  backgroundColor: Colors.white60,
+  appBarTheme: const AppBarTheme(
+    iconTheme: IconThemeData(
+      color: Colors.black87,
+    ),
+    backgroundColor: Colors.white,
+    titleTextStyle: TextStyle(color: Colors.black87),
+  ),
+);
