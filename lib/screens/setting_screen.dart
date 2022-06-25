c0b889ef047f0dddde4e7d0b6de1a26ead383462
Jinwoong <okokrt@gmail.com>
2022-06-25 15:30:23 +0900

    Update Setting Screen UI

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ksubway/main.dart';
@@ -9,6 +7,7 @@ import 'package:flutter_ksubway/preferences/ksubway_stations_preference.dart';
 import 'package:flutter_ksubway/preferences/theme_preference.dart';
 import 'package:flutter_ksubway/services/ksubway_api.dart';
 import 'package:flutter_ksubway/style/textStyles.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -18,17 +17,15 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  KsubwaySeoulstations ksubwaySeoulstations = KsubwaySeoulstations(stationList: []);
   final KsubwayStationsPreference _ksubwayStationsPreference = KsubwayStationsPreference();
-  final KsubwaySeoulstations ksubwaySeoulstations = KsubwaySeoulstations(stationList: []);
   final expApiEndpointTextEditingController = TextEditingController();
   final expApiPreference = ExpApiPreference();
   final themePreference = ThemePreference();
   String expApiEndpoint = "";
   void fetchKsubwayStations() async {
-    KsubwaySeoulstations ksubwaySeoulstations = await _ksubwayStationsPreference.getSeoulstations();
-    if (ksubwaySeoulstations.stationList!.isEmpty) {
-      ksubwaySeoulstations = await KsubwayApi.fetchKsubwayStations(city: 'seoul');
-    }
+    ksubwaySeoulstations = await KsubwayApi.fetchKsubwayStations(city: 'seoul');
+    _ksubwayStationsPreference.setSeoulstations(ksubwaySeoulstations);
   }
 
   void restoreSettings() async {
@@ -47,80 +44,97 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const CupertinoNavigationBar(
-            previousPageTitle: "back",
-          ),
-          Form(
-            autovalidateMode: AutovalidateMode.always,
-            onChanged: () {
-              Form.of(primaryFocus!.context!)?.save();
-            },
-            child: CupertinoFormSection.insetGrouped(
-              header: const Text(
-                'Experimental Section',
-                style: textStyleSub2,
-              ),
-              children: List<Widget>.generate(1, (int index) {
-                return CupertinoTextFormFieldRow(
-                  controller: expApiEndpointTextEditingController..text = expApiEndpoint,
-                  prefix: const Text(
-                    'API Endpoint',
-                    style: textStyleTextFormFieldTitle,
-                  ),
-                  placeholder: 'Enter text',
-                  placeholderStyle: textStyleTextFormFieldContent,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    if (!Uri.parse(value).isAbsolute) return 'Please enter valid api endpoint';
-                  },
-                  onSaved: (value) {
-                    expApiPreference.setApiEndpoint(value ?? "");
-                  },
-                  onEditingComplete: () => expApiEndpointTextEditingController.clear(),
-                );
-              }),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CupertinoNavigationBar(
+              previousPageTitle: "back",
             ),
-          ),
-          Center(
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              onPressed: () async {
-                setState(() {});
-                themePreference.setDarkTheme(MyApp.themeNotifier.value == ThemeMode.light);
-                MyApp.themeNotifier.value = MyApp.themeNotifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+            Form(
+              autovalidateMode: AutovalidateMode.always,
+              onChanged: () {
+                Form.of(primaryFocus!.context!)?.save();
               },
-              icon: Icon(MyApp.themeNotifier.value == ThemeMode.light ? Icons.dark_mode : Icons.light_mode),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: Divider(thickness: 0.2, color: Colors.grey),
-          ),
-          const Spacer(),
-          Container(
-            margin: const EdgeInsets.only(bottom: 8),
-            height: 50,
-            child: Column(
-              children: [
-                const Text(
-                  'Created by dart-bird',
-                  style: textStyleFooter,
+              child: CupertinoFormSection.insetGrouped(
+                header: const Text(
+                  'Experimental Section',
+                  style: textStyleSub2,
                 ),
-                const SizedBox(height: 5),
-                OutlinedButton(
-                  onPressed: () {
-                    showLicensePage(context: context);
-                  },
-                  child: const Text("License", style: textStyleFooter),
-                ),
-              ],
+                children: List<Widget>.generate(1, (int index) {
+                  return CupertinoTextFormFieldRow(
+                    controller: expApiEndpointTextEditingController..text = expApiEndpoint,
+                    prefix: const Text(
+                      'API Endpoint',
+                      style: textStyleTextFormFieldTitle,
+                    ),
+                    placeholder: 'Enter text',
+                    placeholderStyle: textStyleTextFormFieldContent,
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a value';
+                      }
+                      if (!Uri.parse(value).isAbsolute) return 'Please enter valid api endpoint';
+                    },
+                    onSaved: (value) {
+                      expApiPreference.setApiEndpoint(value ?? "");
+                    },
+                    onEditingComplete: () => expApiEndpointTextEditingController.clear(),
+                  );
+                }),
+              ),
             ),
-          ),
-        ],
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    '서울 역정보 업데이트',
+                    style: textStyleSub1,
+                  ),
+                  CupertinoButton(
+                    child: const FaIcon(FontAwesomeIcons.arrowsRotate),
+                    onPressed: () => fetchKsubwayStations(),
+                  )
+                ],
+              ),
+            ),
+            Center(
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () async {
+                  setState(() {});
+                  themePreference.setDarkTheme(MyApp.themeNotifier.value == ThemeMode.light);
+                  MyApp.themeNotifier.value = MyApp.themeNotifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+                },
+                icon: Icon(MyApp.themeNotifier.value == ThemeMode.light ? Icons.dark_mode : Icons.light_mode),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Divider(thickness: 0.2, color: Colors.grey),
+            ),
+            const Spacer(),
+            Container(
+              margin: const EdgeInsets.only(bottom: 8),
+              height: 80,
+              child: Column(
+                children: [
+                  const Text(
+                    'Created by dart-bird',
+                    style: textStyleFooter,
+                  ),
+                  const SizedBox(height: 5),
+                  OutlinedButton(
+                    onPressed: () {
+                      showLicensePage(context: context);
+                    },
+                    child: const Text("License", style: textStyleFooter),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
